18f1cb6617c438a57f0855634b1377b6465d9f46
Jaimie Zhu <jaizhu@umich.edu>
2017-03-05 21:20:05 -0500

    add login view controller

--- FitSquad/FitSquad/LoginViewController.swift
+++ FitSquad/FitSquad/LoginViewController.swift
@@ -1,25 +1,27 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  FitSquad
 //
-//  Created by Jaimie Zhu on 1/22/17.
+//  Created by Jaimie Zhu on 3/5/17.
 //  Copyright © 2017 dogdogdog. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class LoginViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("login view loaded")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
