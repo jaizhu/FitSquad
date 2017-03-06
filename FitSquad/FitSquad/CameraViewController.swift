2258672e0e92e0cabccecc3035f6ec36a6af587b
Rachel Connolly <rshconn@umich.edu>
2017-03-05 23:39:25 -0500

    camera view takes photo

--- FitSquad/FitSquad/CameraViewController.swift
+++ FitSquad/FitSquad/CameraViewController.swift
@@ -7,20 +7,125 @@
 //
 
 import Foundation
+import AVFoundation
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, AVCapturePhotoCaptureDelegate{
+    
+    @IBOutlet weak var captureImageView: UIImageView!
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession!
+    var cameraOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        askPermission()
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        cameraOutput = AVCapturePhotoOutput()
+        
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        if let input = try? AVCaptureDeviceInput(device: device) {
+            if (captureSession.canAddInput(input)) {
+                captureSession.addInput(input)
+                if (captureSession.canAddOutput(cameraOutput)) {
+                    captureSession.addOutput(cameraOutput)
+                    previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                     previewLayer.frame = previewView.bounds
+                     previewView.layer.addSublayer(previewLayer)
+                    captureSession.startRunning()
+                }
+            } else {
+                print("issue here : captureSesssion.canAddInput")
+            }
+        } else {
+            print("some problem here")
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Take picture button
+    @IBAction func didPressTakePhoto(_ sender: UIButton) {
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [
+            kCVPixelBufferPixelFormatTypeKey as String: previewPixelType,
+            kCVPixelBufferWidthKey as String: 160,
+            kCVPixelBufferHeightKey as String: 160
+        ]
+        settings.previewPhotoFormat = previewFormat
+        cameraOutput.capturePhoto(with: settings, delegate: self)
     }
     
+    // callBack from take picture
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if let error = error {
+            print("error occured : \(error.localizedDescription)")
+        }
+        
+        if  let sampleBuffer = photoSampleBuffer,
+            let previewBuffer = previewPhotoSampleBuffer,
+            let dataImage =  AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer:  sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+            print(UIImage(data: dataImage)?.size as Any)
+            
+            let dataProvider = CGDataProvider(data: dataImage as CFData)
+            let cgImageRef: CGImage! = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: .defaultIntent)
+            let image = UIImage(cgImage: cgImageRef, scale: 1.0, orientation: UIImageOrientation.right)
+            
+            self.captureImageView.image = image
+        } else {
+            print("some error here")
+        }
+    }
     
+    // This method you can use somewhere you need to know camera permission   state
+    func askPermission() {
+        print("here")
+        let cameraPermissionStatus =  AVCaptureDevice.authorizationStatus(forMediaType: AVMediaTypeVideo)
+        
+        switch cameraPermissionStatus {
+        case .authorized:
+            print("Already Authorized")
+        case .denied:
+            print("denied")
+            
+            let alert = UIAlertController(title: "Sorry :(" , message: "But  could you please grant permission for camera within device settings",  preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .cancel,  handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+            
+        case .restricted:
+            print("restricted")
+        default:
+            AVCaptureDevice.requestAccess(forMediaType: AVMediaTypeVideo, completionHandler: {
+                [weak self]
+                (granted :Bool) -> Void in
+                
+                if granted == true {
+                    // User granted
+                    print("User granted")
+                    DispatchQueue.main.async(){
+                        //Do smth that you need in main thread
+                    }
+                }
+                else {
+                    // User Rejected
+                    print("User Rejected")
+                    
+                    DispatchQueue.main.async(){
+                        let alert = UIAlertController(title: "WHY?" , message:  "Camera it is the main feature of our application", preferredStyle: .alert)
+                        let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+                        alert.addAction(action)
+                        self?.present(alert, animated: true, completion: nil)  
+                    } 
+                }
+            });
+        }
+    }
 }
+
 
